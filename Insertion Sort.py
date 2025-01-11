5c22a7f82c654ee1fdbf0d8788b1f689cafa7484
bmohammed <bmohammed34470@ucumberlands.edu>
2025-01-11 14:28:19 +0530

    Fourth Commit

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -1,17 +1,24 @@
-#insertion sort
 def insertion_sort_decreasing(arr):
+    # Traverse through 1 to len(arr)
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
-        # Move elements that are smaller than key to one position ahead of their current position
+        
+        # Move elements of arr[0..i-1] that are smaller than key
+        # to one position ahead of their current position
         while j >= 0 and arr[j] < key:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
+
     return arr
 
-# Taking array input from the user
-arr = list(map(int, input("Enter the array elements: ").split()))
+# Taking array input one element at a time
+arr = []
+n = int(input("Enter the number of elements in the array: "))
+for i in range(n):
+    element = int(input(f"Enter element {i + 1}: "))
+    arr.append(element)
 
-sorted_Arr = insertion_sort_decreasing(arr)
-print("Sorted array in monotonically decreasing order:", sorted_Arr)
+sorted_arr = insertion_sort_decreasing(arr)
+print("Sorted array in monotonically decreasing order:", sorted_arr)
