9103ccab76f99c32a061a65f2be64e90683d0d58
bmohammed <bmohammed34470@ucumberlands.edu>
2025-01-11 14:15:11 +0530

    Second Commit

---
+++ Insertion Sort.py
@@ -0,0 +1,17 @@
+#insertion sort
+def insertion_sort_decreasing(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        # Move elements of arr[0..i-1] that are smaller than key to one position ahead of their current position
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+# Taking array input from the user
+arr = list(map(int, input("Enter the array elements: ").split()))
+
+sorted_arr = insertion_sort_decreasing(arr)
+print("Sorted array in monotonically decreasing order:", sorted_arr)
